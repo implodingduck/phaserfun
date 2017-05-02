664919f3f15fd58290635b20b28e74057c6cb83a
implodingduck <implodingduck@users.noreply.github.com>
2017-05-01 21:14:35 -0500

    onclick fun

--- clicker/src/game.js
+++ clicker/src/game.js
@@ -1,16 +1,33 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
  
 game.state.add('play', {
-    preload: function() {
-        game.load.image('skeleton', 'assets/skeleton.png');
-    },
-    create: function() {
-        var skeletonSprite = game.add.sprite(450, 290, 'skeleton');
-        skeletonSprite.anchor.setTo(0.5, 0.5);
-    },
-    render: function() {
-        game.debug.text('Adventure Awaits!', 250, 290);
-    }
+  preload: function() {
+    game.load.image('skeleton', 'assets/skeleton.png');
+    game.load.image('background', 'assets/background.png');
+    game.load.image('sky', 'assets/sky.png');
+  },
+  create: function() {
+    var state = this;
+    this.background = this.game.add.group();
+    this.background.create(0, 300, 'sky').anchor.setTo(0.5, 0.5);
+    this.background.create(0, 500, 'background').anchor.setTo(0.5, 0.5);
+    
+    this.skeletonSprite = game.add.sprite(450, 290, 'skeleton');
+    this.skeletonSprite.anchor.setTo(0.5, 0.5);
+    this.skeletonSprite.tint = 0xff00ff;
+    this.skeletonSprite.inputEnabled = true;
+    this.skeletonSprite.events.onInputDown.add(this.onClickMonster, state);
+  },
+  render: function() {
+    game.debug.text('Adventure Awaits!', 250, 290);
+  },
+  onClickMonster(sprite, pointer){
+    console.log('clicked!');
+    console.log(sprite);
+    sprite.tint = 0x00000a;
+  } 
+ 
 });
  
+
 game.state.start('play');
