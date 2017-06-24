0eefa409a16740d41a80ef077cb68b9df1145283
implodingduck <implodingduck@users.noreply.github.com>
2017-06-24 10:16:51 -0500

    things are moving around now

--- orpg/client/public/js/client.js
+++ orpg/client/public/js/client.js
@@ -1,11 +1,15 @@
+//send stuff
 var client = {
   socket: io.connect('http://127.0.0.1:4200'),
   askNewPlayer: function(){
     this.socket.emit('newplayer');
+  },
+  sendClick: function(x, y){
+    this.socket.emit('click',{x:x,y:y});
   }  
 };
 
-
+//handle responses
 client.socket.on('newplayer',function(data){
     game.state.states['play'].addNewPlayer(data.id,data.x,data.y);
 });
@@ -15,4 +19,12 @@ client.socket.on('allplayers',function(data){
     for(var i = 0; i < data.length; i++){
         game.state.states['play'].addNewPlayer(data[i].id,data[i].x,data[i].y);
     }
+});
+
+client.socket.on('remove', function(playerId){
+  game.state.states['play'].removePlayer(playerId);
+});
+
+client.socket.on('move', function(data){
+  game.state.states['play'].movePlayer(data.id, data.x, data.y);
 });
--- orpg/client/public/js/game.js
+++ orpg/client/public/js/game.js
@@ -1,5 +1,7 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 
+game.players = {};
+
 game.state.add('play', {
   preload: function() {
     game.load.image('skeleton', 'images/skeleton.png');
@@ -12,15 +14,33 @@ game.state.add('play', {
   create: function() {
     var state = this;
     this.background = this.game.add.group();
+    this.background.inputEnableChildren = true;
     this.background.create(0, 300, 'sky').anchor.setTo(0.5, 0.5);
     this.background.create(0, 500, 'background').anchor.setTo(0.5, 0.5);
     client.askNewPlayer();
+    this.background.onChildInputUp.add(this.getCoordinates, this);
   },
   render: function() {
     game.debug.text('Adventure Awaits!', 250, 290);
   },
   addNewPlayer: function (id, x, y){
-    game.add.text(x, y, id, { font: "15px Arial", fill: "#ff00ff" });
+    game.players[id] = game.add.text(x, y, id, { font: "15px Arial", fill: "#ff00ff" });
+  },
+  removePlayer: function (id){
+    game.players[id].destroy();
+    delete game.players[id];
+  },
+  getCoordinates: function (layer, pointer){
+    console.log('click!')
+    client.sendClick(pointer.worldX,pointer.worldY);
+  },
+  movePlayer: function (id, x, y){
+    var player = game.players[id];
+    var distance = Phaser.Math.distance(player.x,player.y,x,y);
+    var duration = distance*10;
+    var tween = game.add.tween(player);
+    tween.to({x:x,y:y}, duration);
+    tween.start();
   }
 
  
